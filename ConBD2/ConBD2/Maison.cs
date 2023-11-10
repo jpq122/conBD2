19d9695c30b1da287e95b9c15f8a2bf7472bfbd6
jpq122 <ndjondo.agance20@yahoo.com>
2023-11-10 13:11:20 -0500

    les classes

---
+++ ConBD2/ConBD2/Maison.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConBD2
+{
+    internal class Maison
+    {
+            
+            private int ID;
+            private String categorie;
+            private double prix;
+            private String ville;
+            private int proprietaireID;
+
+            public Maison(int ID, String categorie, double prix, String ville, int proprietaireID)
+            {
+                this.ID = ID;
+                this.categorie = categorie;
+                this.prix = prix;
+                this.ville = ville;
+                this.proprietaireID = proprietaireID;
+            }
+
+            
+    }
+
+}
---
+++ ConBD2/ConBD2/Propriétaire.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConBD2
+{
+    internal class Propriétaire
+    {
+        public class Proprietaire
+        {
+            private int ID;
+            private String nom;
+            private String prenom;
+
+            public Proprietaire(int ID, String nom, String prenom)
+            {
+                this.ID = ID;
+                this.nom = nom;
+                this.prenom = prenom;
+            }  
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
