ca5753f8c4e5ef32ba085fe3682f8d0ed7df7201
jpq122 <ndjondo.agance20@yahoo.com>
2023-11-10 12:43:47 -0500

    Ajout sln

--- ConBD2/ConBD2/App.xaml.cs
+++ ConBD2/ConBD2/App.xaml.cs
@@ -19,7 +19,7 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace conBD2
+namespace ConBD2
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
